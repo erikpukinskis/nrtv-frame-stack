065216265143708f0ed415fd3c0b74b77fc1cbfa
Erik Pukinskis <erik@snowedin.net>
2015-06-17 21:45:20 -0700

    Refactor so we reuse the frame positions if there are no reset

--- collectives.js
+++ collectives.js
@@ -25,23 +25,35 @@ define(function() {
       this.stacks[name] = [clone(attributes)]
     }
 
+  function getCollective(stacks, positions, name) {
+    return stacks[name][positions[name]]    
+  }
+
   Collectives.prototype.getFrame =
     function(resets) {
       var stacks = this.stacks
-      var positions = clone(this.framePositions)
+      var noResets = !resets || resets.length < 1
 
-      for(var i=0; resets && i<resets.length; i++) {
+      var positions = this.framePositions
+
+      if (noResets) {
+        return getCollective.bind(
+          null, stacks, positions
+        )
+      }
+
+      positions = clone(positions)
+
+      for(var i=0; i<resets.length; i++) {
         var name = resets[i]
         var collective = clone(this.defaults[name])
         var newPosition = stacks[name].push(collective) - 1
         positions[name] = newPosition
       }
 
-      function getCollective(name) {
-        return stacks[name][positions[name]]
-      }
-
-      return getCollective
+      return getCollective.bind(
+        null, stacks, positions
+      )
     }
 
   return Collectives
