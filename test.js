a820a0666ac0b6d0f696b093da3585b9fa07b24f
Erik Pukinskis <erik@snowedin.net>
2015-06-17 22:07:14 -0700

    Rename to frame-stack and change test to use callback style

--- test.js
+++ test.js
@@ -2,44 +2,58 @@ requirejs = require("requirejs")
 var chai = require("chai")
 
 requirejs(
-  ["collectives"],
-  function(Collectives) {
+  ["frame-stack"],
+  function(FrameStack) {
     var expect = chai.expect
 
-    var collectives = new Collectives()
+    function stackEggsAndBeans() {
+      var food = new FrameStack()
 
-    collectives.default("egg", {yolkCounts: []})
+      food.default("egg", {yolkCounts: []})
 
-    collectives.default("bean", {sproutedCount: 0})
+      food.default("bean", {sproutedCount: 0})
 
-    var firstFrame = collectives.getFrame()
+      var firstFrame
+      var secondFrame
 
-    firstFrame("egg").yolkCounts.push(1)
-    firstFrame("egg").yolkCounts.push(1)
+      food.frame(eggsAndASprout)      
+    }
 
-    firstFrame("bean").sproutedCount++
+    function eggsAndASprout(frame) {
+      firstFrame = frame
 
-    var secondFrame = collectives.getFrame(["bean"])
+      firstFrame("egg").yolkCounts.push(1)
+      firstFrame("egg").yolkCounts.push(1)
 
-    firstFrame("egg").yolkCounts.push(2)
+      firstFrame("bean").sproutedCount++
 
-    expect(firstFrame("egg").yolkCounts)
-    .to.deep.equal([1,1,2])
+      food.frame(["bean"], funkyYolks)
+    }
 
-    expect(secondFrame("egg").yolkCounts)
-    .to.deep.equal([1,1,2])
+    function funkyYolks(frame) {
+      secondFrame = frame
 
-    expect(firstFrame("bean").sproutedCount).to.equal(1)
+      firstFrame("egg").yolkCounts.push(2)
 
-    expect(secondFrame("bean").sproutedCount).to.equal(0)
+      expect(firstFrame("egg").yolkCounts)
+      .to.deep.equal([1,1,2])
 
-    secondFrame("egg").yolkCounts.push(1)
+      expect(secondFrame("egg").yolkCounts)
+      .to.deep.equal([1,1,2])
 
-    expect(firstFrame("egg").yolkCounts)
-    .to.deep.equal([1,1,2,1])
+      expect(firstFrame("bean").sproutedCount).to.equal(1)
 
-    expect(secondFrame("egg").yolkCounts)
-    .to.deep.equal([1,1,2,1])
+      expect(secondFrame("bean").sproutedCount).to.equal(0)
 
-    console.log("durrrrr")
+      secondFrame("egg").yolkCounts.push(1)
+
+      expect(firstFrame("egg").yolkCounts)
+      .to.deep.equal([1,1,2,1])
+
+      expect(secondFrame("egg").yolkCounts)
+      .to.deep.equal([1,1,2,1])
+
+      console.log("durrrrr")
+    }
+
 })
